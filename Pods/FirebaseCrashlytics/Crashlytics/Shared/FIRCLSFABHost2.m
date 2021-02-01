747ba71809130631349bb6f18ac82d0d59f17cc0
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2021-02-01 10:34:03 +0100

    Another similar file

---
+++ Pods/FirebaseCrashlytics/Crashlytics/Shared/FIRCLSFABHost2.m
@@ -0,0 +1,57 @@
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
+
+@interface MyClass:NSObject { 
+   // class variable declared here
+}
+
+// class properties declared here
+// class methods and instance methods declared here
+@end
